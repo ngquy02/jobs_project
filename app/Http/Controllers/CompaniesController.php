ca850fcdd16eb241d130807b1ba638ff16bc4e29
Nguyen Dinh Quy <dinhquy4869@gmail.com>
2023-11-06 16:11:07 +0700

    first_commit

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -7,74 +7,74 @@ use App\Models\Companies;
 
 class CompaniesController extends Controller
 {
-        //GET Method
-        public function index()
+    //GET Method
+    public function index()
+    {
+        $companies = Companies::all();
+        return response()->json($companies);
+    }
+
+    //GET Method
+    public function show($id)
+    {
+        $company = Companies::find($id);
+
+        if (!$company)
         {
-            $companies = Companies::all();
-            return response()->json($companies);
+            return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
         }
-    
-        //GET Method
-        public function show($id)
+
+        return response()->json($company);
+    }
+
+    //POST Method
+    public function store(Request $request)
+    {
+        $company = new Companies();
+        $company -> name = $request -> name;
+        $company -> slug = $request -> authorId;
+        $company -> logo = $request -> jobId;
+        $company -> url = $request -> url;
+        $company -> industry = $request -> industry;
+        $company -> twitter = $request -> twitter;
+        $company -> bio = $request -> bio;
+        $company -> createdAt = $request -> createdAt;
+        $company -> updatedAt = $request -> updatedAt;
+        $company -> isArchived = $request -> isArchived;
+        $company -> isActive = $request -> isActive;
+        $company->save();
+
+        return response()->json(['message' => 'Đã tạo thành công.', 'company' => $company], 201);
+    }
+
+    //PUT Method
+    public function update(Request $request, $id)
+    {
+        $company = Companies::find($id);
+
+        if (!$company)
         {
-            $company = Companies::find($id);
-    
-            if (!$company)
-            {
-                return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
-            }
-    
-            return response()->json($company);
+            return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
         }
-    
-        //POST Method
-        public function store(Request $request)
+
+        $data = $request->all();
+        $company->update($data);
+
+        return response()->json(['message' => 'Cập nhật thành công.', 'company' => $company]);
+    }
+
+    //DELETE Method
+    public function destroy($id)
+    {
+        $company = Companies::find($id);
+
+        if (!$company)
         {
-            $company = new Companies();
-            $company -> name = $request -> name;
-            $company -> slug = $request -> authorId;
-            $company -> logo = $request -> jobId;
-            $company -> url = $request -> url;
-            $company -> industry = $request -> industry;
-            $company -> twitter = $request -> twitter;
-            $company -> bio = $request -> bio;
-            $company -> createdAt = $request -> createdAt;
-            $company -> updatedAt = $request -> updatedAt;
-            $company -> isArchived = $request -> isArchived;
-            $company -> isActive = $request -> isActive;
-            $company->save();
-    
-            return response()->json(['message' => 'Đã tạo thành công.', 'company' => $company], 201);
+            return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
         }
-    
-        //PUT Method
-        public function update(Request $request, $id)
-        {
-            $company = Companies::find($id);
-    
-            if (!$company)
-            {
-                return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
-            }
-    
-            $data = $request->all();
-            $company->update($data);
-    
-            return response()->json(['message' => 'Cập nhật thành công.', 'company' => $company]);
-        }
-    
-        //DELETE Method
-        public function destroy($id)
-        {
-            $company = Companies::find($id);
-    
-            if (!$company)
-            {
-                return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
-            }
-    
-            $company->delete();
-    
-            return response()->json(['message' => 'Xóa thành công.']);
-        }
+
+        $company->delete();
+
+        return response()->json(['message' => 'Xóa thành công.']);
+    }
 }
--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -3,8 +3,78 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Jobs;
 
 class JobsController extends Controller
 {
-    //
+    //GET Method
+    public function index()
+    {
+        $companies = Jobs::all();
+        return response()->json($companies);
+    }
+
+    //GET Method
+    public function show($id)
+    {
+        $company = Jobs::find($id);
+
+        if (!$company)
+        {
+            return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
+        }
+
+        return response()->json($company);
+    }
+
+    //POST Method
+    public function store(Request $request)
+    {
+        $company = new Jobs();
+        $company -> name = $request -> name;
+        $company -> slug = $request -> authorId;
+        $company -> logo = $request -> jobId;
+        $company -> url = $request -> url;
+        $company -> industry = $request -> industry;
+        $company -> twitter = $request -> twitter;
+        $company -> bio = $request -> bio;
+        $company -> createdAt = $request -> createdAt;
+        $company -> updatedAt = $request -> updatedAt;
+        $company -> isArchived = $request -> isArchived;
+        $company -> isActive = $request -> isActive;
+        $company->save();
+
+        return response()->json(['message' => 'Đã tạo thành công.', 'company' => $company], 201);
+    }
+
+    //PUT Method
+    public function update(Request $request, $id)
+    {
+        $company = Jobs::find($id);
+
+        if (!$company)
+        {
+            return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
+        }
+
+        $data = $request->all();
+        $company->update($data);
+
+        return response()->json(['message' => 'Cập nhật thành công.', 'company' => $company]);
+    }
+
+    //DELETE Method
+    public function destroy($id)
+    {
+        $company = Jobs::find($id);
+
+        if (!$company)
+        {
+            return response()->json(['message' => 'Bản ghi không tồn tại.'], 404);
+        }
+
+        $company->delete();
+
+        return response()->json(['message' => 'Xóa thành công.']);
+    }
 }
